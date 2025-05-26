8bead98f37796063e0bd2a965cb7c0ca638afa85
TonyCross23 <tonychamp40@gmail.com>
2025-05-26 21:37:56 +0630

    action

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/my-potfolio/",
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/my-potfolio/',
   plugins: [react()],
-})
+}))
